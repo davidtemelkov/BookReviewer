cf84bd3eab1cba290e597174e23633937e852d1f
davidtemelkov <dntemelkov@gmail.com>
2021-08-02 11:26:25 +0300

    Scaffolded default identity

---
+++ src/BookReviewer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BookReviewer.Data;
+using BookReviewer.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookReviewer.Areas.Identity.IdentityHostingStartup))]
+namespace BookReviewer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
