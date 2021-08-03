5e6056245ee590a9a8f31e1c3d5a43d20e5c602a
davidtemelkov <dntemelkov@gmail.com>
2021-08-03 21:43:47 +0300

    Added User and Author relationship

---
+++ src/BookReviewer/Data/Migrations/20210803183113_AddedAuthorIdAndIsAuthorToUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookReviewer.Migrations
+{
+    public partial class AddedAuthorIdAndIsAuthorToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthor",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAuthor",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/BookReviewer/Data/Models/User.cs
+++ src/BookReviewer/Data/Models/User.cs
@@ -9,6 +9,10 @@
     {
         public string ProfilePicture { get; set; } = UserDefaultProfilePicture;
 
+        public int? AuthorId { get; set; }
+
+        public bool IsAuthor { get; set; }
+
         public ICollection<Review> Reviews { get; init; } = new List<Review>();
 
         public ICollection<List> Lists { get; init; } = new List<List>();
